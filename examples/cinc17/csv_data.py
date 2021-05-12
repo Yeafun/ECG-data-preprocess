88b5573920dceaa1f2551509a5d4d468e3e5a44a
Yeafun <754137166@qq.com>
2021-05-12 10:44:05 +0800

    process data to csv format

--- examples/cinc17/csv_data.py
+++ examples/cinc17/csv_data.py
@@ -52,29 +52,64 @@ def make_json(save_path, dataset):
             json.dump(datum, fid)
             fid.write('\n')
 
+
+def process_y(y):
+    classes = ['N', 'O', '~', 'A']
+    int_to_class = dict( zip(range(len(classes)), classes))
+    # print(int_to_class)
+    class_to_int = {c : i for i, c in int_to_class.items()}
+    # print(class_to_int)
+    y = class_to_int[y]
+    return y
+
+def compute_mean_std(x):
+    x = np.hstack(x)
+    return (np.mean(x).astype(np.float32),
+        np.std(x).astype(np.float32))
+
+
+
+def process_x(x,mean,std):
+    # max_len = max(len(i) for i in x)
+    # padded = np.full((len(x), max_len), 0, dtype=np.float32)
+    # for e, i in enumerate(x):
+    #     padded[e, :len(i)] = i
+    # x = padded(x)
+    x = (x - mean) / std
+   
+    return x
+
 def make_csv(save_path, dataset):
     import sys
     sys.path.append("../../ecg")
     import load
+    ecg = []
     with open(save_path, 'w') as fid:
-        f_csv = csv.writer(fid, delimiter=' ')
+        for d in dataset:
+            ecg_dir = d[0]
+            ecg.append(load.load_ecg(ecg_dir))
+    mean, std = compute_mean_std(ecg)
+
+    print("mean: ",mean)
+    print("std: ", std)
+    with open(save_path, 'w') as fid:
+        f_csv = csv.writer(fid, delimiter='\t')
         tag = ["label", "text"]
         f_csv.writerow(tag)
+
         for d in dataset:
             ecg_dir = d[0]
             data = load.load_ecg(ecg_dir)
             label = d[1][0]
+            data = process_x(data, mean, std)
+            label = process_y(label)
 
-            label_data = []
-            label_data.append(label)
-            for x in data:
-                label_data.append(x)
-            
+            label_data = []            
+            label_data.append(label)       
+            str1 = ' '.join([str(da) for  da in data])         
+            label_data.append(str1)       
             f_csv.writerow(label_data)
             
-            
-
-
 
 
 
@@ -86,11 +121,10 @@ if __name__ == "__main__":
     dataset = load_all(data_path)
 
     train, dev = split(dataset, dev_frac)
-    print(type(train))
-    print(len(train))
-    print(train[0])
-    print(dev[0])
+    
+
     make_csv("train.csv", train)
+    make_csv("dev.csv", dev)
 
     # make_json("train.json", train)
     # make_json("dev.json", dev)
